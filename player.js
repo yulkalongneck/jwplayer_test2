72e6ba3301fc708e8b62be2e2c38f8fdab1f031e
Yolka <yolka@RWCEncoder1s-MacBook-Pro.local>
2017-02-17 10:08:44 -0800

    Add valid validation for video length.

--- player.js
+++ player.js
@@ -25,36 +25,16 @@ $(document).ready(function() {
       jwplayer("SDMPlayer").play();
     });
 
-    // I can see the value of var duartion appended to div, but I cannot figure out howe to save it a variable;
-    // $("#play").on("click", function(){
-    //   jwplayer("SDMPlayer").play();
-    //   jwplayer("SDMPlayer").onTime(function(){
-    //     var duration = jwplayer("SDMPlayer").getDuration();
-    //     $("#durationText").append(duration);
-    //   });
-    // });
-  
-    // autoplay while loading tha page th=en pause it.
 
-    // jwplayer("SDMPlayer").play();
-    //   $("#durationText").append(jwplayer("SDMPlayer").getState(), jwplayer("SDMPlayer").getDuration())
-      
-      
-    // jwplayer('SDMPlayer').pause();
-    //   $("#durationText").append(jwplayer('SDMPlayer').getState(), jwplayer('SDMPlayer').getDuration())
     var video = jwplayer('SDMPlayer');
-
     var duration;
         video.on('bufferChange', function (){
         duration = video.getDuration();
-        $("#durationText").append(duration);
+        // $("#durationText").append(duration);
     });
 
 
-
-
     var addChapter = document.getElementById('add-chapter');
-
         // hours
     var hours = $(function() {
       var $timeHours = $("#chapter-timing-hours");
@@ -87,7 +67,6 @@ $(document).ready(function() {
     addChapter.addEventListener("click", function() {
 
       var name = document.getElementById('chapter-name').value;
-        // console.log(name);
       var $Hours = (document.getElementById("hours").value = document.getElementById("chapter-timing-hours").value);
       var $Minutes = (document.getElementById("minutes").value = document.getElementById("chapter-timing-minutes").value);
       var $Seconds = (document.getElementById("seconds").value = document.getElementById("chapter-timing-seconds").value);
@@ -98,12 +77,10 @@ $(document).ready(function() {
         alert("Name can't be blank or longer than 20 characters.");
       } else if (name.match(/^[0-9]/)) {
         alert("Name format is invalid! Name can't start with a digit!");
-        // !!!!!!!!!!!
+        // time validation
       } else if (time > duration) {
         alert("This video is not long enough!");
-        video.stop();
-        // video.pause();
-      //   // time validation
+      // time validation
       } else if (time == 0) {
         alert("Timing can't be equal to 0!");
       } else {
@@ -116,11 +93,8 @@ $(document).ready(function() {
       };
     });
 
-    var duration;
     $("#play").on("click", function(){
       jwplayer("SDMPlayer").play();
-      duration = jwplayer("SDMPlayer").getDuration();
-      $("#durationText").append(duration);
     });
 
     $("#pause").on("click", function(){
@@ -128,81 +102,6 @@ $(document).ready(function() {
     });
 
 });
-
-
-// // hours
-// var hours = $(function() {
-//   var $timeHours = $("#chapter-timing-hours");
-//   for (var i = 1; i <= 3; i++) {
-//     $timeHours.append($('<option value='+ i +'> 0'+i+'</option>'));
-//   }
-// });
-// //minutes
-// var minutes = $(function() {
-//   var $timeMinutes = $("#chapter-timing-minutes");
-//   for (var i = 1; i <= 9; i++) {
-//     $timeMinutes.append($('<option value='+ i +'> 0'+i+'</option>'));
-//   }
-//   for (var i = 10; i <= 59; i++) {
-//     $timeMinutes.append($('<option value='+ i +'>'+i+'</option>'));
-//   }
-// });
-// // seconds
-// var seconds = $(function() {
-//   var $timeSeconds = $("#chapter-timing-seconds");
-//   for (var i = 1; i <= 9; i++) {
-//     $timeSeconds.append($('<option value='+ i +'> 0'+i+'</option>'));
-//   }
-//   for (var i = 10; i <= 59; i++) {
-//     $timeSeconds.append($('<option value='+ i +'>'+i+'</option>'));
-//   }
-// });
-
-
-// var addChapter = document.getElementById('add-chapter');
-// var video = jwplayer('SDMPlayer');
-
-// addChapter.addEventListener("click", function() {
-
-//   var duration;
-
-//     duration = video.getDuration();
-//     video.play();
-//     duration = video.getDuration();
-
-//     video.pause();
-//     $("#durationText").append(duration);
-
-//   var name = document.getElementById('chapter-name').value;
-//     // console.log(name);
-//   var $Hours = (document.getElementById("hours").value = document.getElementById("chapter-timing-hours").value);
-//   var $Minutes = (document.getElementById("minutes").value = document.getElementById("chapter-timing-minutes").value);
-//   var $Seconds = (document.getElementById("seconds").value = document.getElementById("chapter-timing-seconds").value);
-//   var time = ((parseInt($Hours) * 360) + (parseInt($Minutes) * 60) + (parseInt($Seconds)));
-
-//   // name validation
-//   if ((name == "") || (name.length > 20)) {
-//     alert("Name can't be blank or longer than 20 characters.");
-//   } else if (name.match(/^[0-9]/)) {
-//     alert("Name format is invalid! Name can't start with a digit!");
-//     // !!!!!!!!!!!
-//   } else if (time > duration) {
-//     alert("This video is not long enough!");
-//     video.stop();
-//     // video.pause();
-//   //   // time validation
-//   } else if (time == 0) {
-//     alert("Timing can't be equal to 0!");
-//   } else {
-//     var nameid = name.replace(/\s+/g, "-");
-//     $('#new-chapter').append('<button id=' + nameid + '>' + name + '</button>' + time + 'sec' + '<br>');
-//     $('#' + nameid).click(function() {
-//       video.seek(time);
-//       event.preventDefault();
-//     });
-//   };
-
-// });
 
 
 
